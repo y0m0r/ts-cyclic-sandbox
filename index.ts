eaafb912d7aa87afc45d7495dd3365f472561c2b
Yuhei Omori <yuheiomori@gmail.com>
2022-09-18 17:49:01 +0900

    build

--- index.ts
+++ index.ts
@@ -5,7 +5,8 @@ import * as http from "http";
 const server = http.createServer(
     (request, response) => {
       // サーバーにリクエストがあった時に実行される関数
-      response.end("Yo Yo Yo!");
+
+      response.end("Yo Yo Yo Yo!");
     }
   );
   // サーバーを起動してリクエストを待ち受け状態にする
