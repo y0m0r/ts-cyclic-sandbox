ed95dcf64745bf22934d4086b7bc36ed7e3da18d
Yuhei Omori <yuheiomori@gmail.com>
2022-09-18 18:47:26 +0900

    dist

--- index.js
+++
@@ -1,7 +0,0 @@
-import * as http from "http";
-const server = http.createServer(
-  (request, response) => {
-    response.end("Yo Yo Yo!");
-  }
-);
-server.listen(process.env.PORT || 3e3);
--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const app: express.Express = express()
 
 app.all('/', (req: express.Request, res: express.Response) => {
   console.log("Just got a request!")
-  res.send('Express Yo Yo Yo Yo!')
+  res.send('Express Yo Yo Yo Yo! Dist Dist!')
 })
 app.listen(process.env.PORT || 3000)
 
