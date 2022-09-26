f4c0f45e67264111b711b09a4816ca85b56a7c44
Yuhei Omori <yuheiomori@gmail.com>
2022-09-26 17:07:58 +0900

    update message

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const app: express.Express = express()
 
 app.all('/', (req: express.Request, res: express.Response) => {
   console.log("Just got a request!")
-  res.send('Express Yo Yo Yo Yo! Dist Dist!')
+  res.send('Hello, Cyclic World!')
 })
 app.listen(process.env.PORT || 3000)
 
